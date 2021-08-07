19471ffeeee3ee09d821cb84dcce41f7448735c5
Ishan Makadia <i5han3.dev@gmail.com>
2021-08-07 16:06:07 +0530

    chore: remove pet app project

--- react/adopt-me/src/ThemeContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-
-// createContext is a function that returns an object with two React components in it: a Provider and a Consumer
-const ThemeContext = createContext(["green", () => { }]);
-
-export default ThemeContext;
